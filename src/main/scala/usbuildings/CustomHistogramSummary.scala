ae17d9b3c41c693932d9851df2f8abaaaf8c4845
Eugene Cheipesh <echeipesh@gmail.com>
2019-05-16 10:21:53 -0400

    Fully type handler to StreamingHistogram

--- src/main/scala/usbuildings/CustomHistogramSummary.scala
+++ src/main/scala/usbuildings/CustomHistogramSummary.scala
@@ -6,8 +6,8 @@ import geotrellis.raster.rasterize.Rasterizer
 import geotrellis.raster.summary.polygonal.TilePolygonalSummaryHandler
 import geotrellis.vector.Polygon
 
-object CustomDoubleHistogramSummary extends TilePolygonalSummaryHandler[Histogram[Double]] {
-  def handlePartialTile(raster: Raster[Tile], polygon: Polygon): Histogram[Double] = {
+object CustomDoubleHistogramSummary extends TilePolygonalSummaryHandler[StreamingHistogram] {
+  def handlePartialTile(raster: Raster[Tile], polygon: Polygon): StreamingHistogram = {
     val Raster(tile, _) = raster
     val rasterExtent = raster.rasterExtent
     val histogram = StreamingHistogram()
@@ -20,13 +20,13 @@ object CustomDoubleHistogramSummary extends TilePolygonalSummaryHandler[Histogra
     histogram
   }
 
-  def handleFullTile(tile: Tile): Histogram[Double] = {
+  def handleFullTile(tile: Tile): StreamingHistogram = {
     val histogram = StreamingHistogram()
     tile.foreach { (z: Int) => if (isData(z)) histogram.countItem(z, 1) }
     histogram
   }
 
-  def combineResults(rs: Seq[Histogram[Double]]): Histogram[Double] =
+  def combineResults(rs: Seq[StreamingHistogram]): StreamingHistogram =
     if (rs.nonEmpty) rs.reduce(_ merge _)
     else StreamingHistogram()
 }
